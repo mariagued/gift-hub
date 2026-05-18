ed429f4191f0b399a3d241578c64a25225eea36c
Ane <goncalvesanne961@gmail.com>
2026-05-18 19:50:18 -0300

    chore: adiciona regras modulares de IA e config de globbing no workspace

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/layout/header/header.component';
 import { SidenavComponent } from './core/layout/sidenav/sidenav.component';
 import { FooterComponent } from './core/layout/footer/footer.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SidenavComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, SidenavComponent, FooterComponent, UserProfileComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent { }
