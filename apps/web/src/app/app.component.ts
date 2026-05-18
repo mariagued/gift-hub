50259d2d22524f64f55a7f19f4c40454b4ea0fda
Maria Eduarda Guedes <mariaed.gueds@gmail.com>
2026-05-18 02:46:37 -0300

    feat: cria app shell com header, sidenav, footer e layout base

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { SidenavComponent } from './core/layout/sidenav/sidenav.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    template: '<router-outlet></router-outlet>'
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent, SidenavComponent, FooterComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent { }
