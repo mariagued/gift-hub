b3b774aac4afe21e51ed8232741804f10598597b
Ane <goncalvesanne961@gmail.com>
2026-06-24 23:12:27 -0300

    ci: configure Jest for monorepo and fix Router mocks in spec files

--- apps/web/src/app/features/auth/register/register.component.spec.ts
+++ apps/web/src/app/features/auth/register/register.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
@@ -8,7 +8,8 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegisterComponent]
+      imports: [RegisterComponent],
+      providers: [provideRouter([])]
     })
     .compileComponents();
 
