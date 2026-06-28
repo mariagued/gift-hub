28b0180473fcf2d95c12a4f342c17b9ea2de647a
Ane <goncalvesanne961@gmail.com>
2026-06-28 17:09:57 -0300

    chore: enable hash routing to prevent 404 on github pages

--- apps/web/src/environments/environment.ts
+++ apps/web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:3001',
+  apiUrl: 'https://mariagued.github.io/gift-hub/',
   supabaseUrl: 'https://wpyoizomsampbqhcwrkt.supabase.co',
   supabaseKey: 'sb_publishable_5dWHbGKWJRU8s1S33KSn5g_Ez8CTWCy'
 };
